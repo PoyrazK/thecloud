4dddfc477177a545174958fd3958096b78492398
github-action-benchmark <github@users.noreply.github.com>
2026-01-09 12:13:29 +0000

    add Go Benchmarks (go) benchmark result for 66677b5d976641a5e9eb591059f9944016e4a515

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767960809200,
+  "repoUrl": "https://github.com/PoyrazK/thecloud",
+  "entries": {
+    "Go Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "83272398+PoyrazK@users.noreply.github.com",
+            "name": "Poyraz Küçükarslan",
+            "username": "PoyrazK"
+          },
+          "committer": {
+            "email": "83272398+PoyrazK@users.noreply.github.com",
+            "name": "Poyraz Küçükarslan",
+            "username": "PoyrazK"
+          },
+          "distinct": true,
+          "id": "66677b5d976641a5e9eb591059f9944016e4a515",
+          "message": "ci: add write permissions to benchmarks workflow",
+          "timestamp": "2026-01-09T15:12:58+03:00",
+          "tree_id": "fae73ef4a542441e13f73b3fe8b43bbb8a95e077",
+          "url": "https://github.com/PoyrazK/thecloud/commit/66677b5d976641a5e9eb591059f9944016e4a515"
+        },
+        "date": 1767960802284,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInstanceService_List",
+            "value": 1.895,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "639451438 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceService_List - ns/op",
+            "value": 1.895,
+            "unit": "ns/op",
+            "extra": "639451438 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceService_List - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "639451438 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceService_List - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "639451438 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCService_Get",
+            "value": 138.7,
+            "unit": "ns/op\t     192 B/op\t       2 allocs/op",
+            "extra": "9101067 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCService_Get - ns/op",
+            "value": 138.7,
+            "unit": "ns/op",
+            "extra": "9101067 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCService_Get - B/op",
+            "value": 192,
+            "unit": "B/op",
+            "extra": "9101067 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCService_Get - allocs/op",
+            "value": 2,
+            "unit": "allocs/op",
+            "extra": "9101067 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
