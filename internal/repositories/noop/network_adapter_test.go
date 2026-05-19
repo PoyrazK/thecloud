bf049a4c3b03958274abb693c8ccc20b2e065217
poyrazK <83272398+poyrazK@users.noreply.github.com>
2026-05-19 12:25:46 +0300

    fix: add require import to noop test files

--- internal/repositories/noop/network_adapter_test.go
+++ internal/repositories/noop/network_adapter_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/poyrazk/thecloud/internal/core/ports"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNoopNetworkAdapter_Type(t *testing.T) {
